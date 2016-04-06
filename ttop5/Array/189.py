4230ecc08014c81c8ec55f6a863a59a4e81e5dd4
ttop5 <ttop5@qq.com>
2016-04-06 18:53:12 +0800

    189. Rotate Array

--- ttop5/Array/189.py
+++ ttop5/Array/189.py
@@ -1,3 +1,4 @@
+# 方法一：
 class Solution(object):
 
     def rotate(self, nums, k):
@@ -5,6 +6,7 @@ class Solution(object):
         :type nums: List[int]
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
+        LeetCode上报错的数据（[1,2],1）在本地没有问题啊，擦
         """
         nList = []
         i = len(nums) - k
@@ -14,3 +16,18 @@ class Solution(object):
         for j in range(len(nums) - k):
             nList.append(nums[j])
         nums = nList
+
+
+# 方法二：
+class Solution(object):
+
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        和上面方法思路一样，只是巧妙利用列表切片
+        """
+        L = len(nums)
+        if k > 0 and L > 1:
+            nums[:] = nums[L - k:] + nums[:L - k]
