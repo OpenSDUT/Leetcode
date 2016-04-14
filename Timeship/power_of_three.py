7584b4c3b7c70a21c4e53cbc8ae618e56242ef0d
Timeship <leonwowo@gmail.com>
2016-04-14 22:03:52 +0800

    power_of_three

---
+++ Timeship/power_of_three.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-
+import math
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        #判断一个数是不是三的幂，递归
+        if n == 0:
+        	return False
+        elif n == 1:
+        	return True
+        else:
+        	if(n%3 == 0):
+        		return self.isPowerOfThree(n/3)
+        	else:
+        		return False
+    def isPowerOfThree2(self,n):
+    	if n<=0:
+    		return False
+    	logx = math.log10(n)/math.log10(3)
+    	return True if logx-int(logx) == 0 else False
+
+if __name__ == '__main__':
+	test = Solution()
+	print(test.isPowerOfThree2(243))
